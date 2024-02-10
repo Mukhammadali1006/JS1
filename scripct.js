f54bc2ef688de8ad96dd7eb377e10c812deb413f
Mukhammadali <turaevmukhammadali81@gmail.com>
2024-02-10 17:54:22 +0500

    js

---
+++ scripct.js
@@ -0,0 +1,17 @@
+var ism = prompt(" ismingizni kiriting")
+console.log("ism:" + ism);
+
+var yosh = +prompt("yoshingizni kiriting")
+console.log("yosh:" + yosh);
+
+var misol = +prompt ("10-2=")
+console.log("sizning javob:"+misol +" togri javob :"+(10-2));
+
+var misol1 = +prompt ("10+2=")
+console.log("sizning javob:"+misol1 +" togri javob :"+(10+2));
+
+var misol2 = +prompt ("10*2=")
+console.log("sizning javob:"+misol2 +" togri javob :"+(10*2));
+
+var misol3 = +prompt ("10/2=")
+console.log("sizning javob:"+misol3 +" togri javob :"+(10/2));
